1c788e699248f90ee66935913765288ab4ec92f1
Elad Hoffer <elad.hoffer@gmail.com>
2015-06-09 19:09:54 +0300

    compressed lmdb

--- Config.lua
+++ Config.lua
@@ -3,18 +3,25 @@ for i=1001,#ImageNetClasses.ClassName do
     ImageNetClasses.ClassName[i] = nil
 end
 
+function Key(num)
+    return string.format('%07d',num)
+end
+
+
 return
 {
     TRAINING_PATH = '/home/ehoffer/Datasets/ImageNet/train/',
     VALIDATION_PATH = '/home/ehoffer/Datasets/ImageNet/validation/',
-    VALIDATION_DIR = './Data/ValidationCache/',
-    TRAINING_DIR = './Data/TrainingCache/',
+    VALIDATION_DIR = '/home/ehoffer/Datasets/ImageNet/LMDB_Compressed/validation/',
+    TRAINING_DIR = '/home/ehoffer/Datasets/ImageNet/LMDB_Compressed/train/',
     ImageSize = 256,
-    SampleSize = {3,256,256},
+    SampleSize = {3,224,224},
     ValidationLabels = torch.load('./ValidationLabels'),
     ImageNetClasses = ImageNetClasses,
     DataMean = 118.380948,
-    DataStd = 61.896913
+    DataStd = 61.896913,
+    Compressed = true,
+    Key = Key
 }
 
 
--- CreateLMDBs.lua
+++ CreateLMDBs.lua
@@ -11,7 +11,8 @@ local config = require 'Config'
 -------------------------------Settings----------------------------------------------
 
 local PreProcess = function(Img)
-    local im = CropCenter(Img,config.ImageSize)
+    local im = image.scale(Img, '^' .. config.ImageSize) --minimum side of ImageSize
+
     if im:dim() == 2 then
         im = im:reshape(1,im:size(1),im:size(2))
     end
@@ -26,80 +27,85 @@ end
 
 
 local LoadImgData = function(filename)
-    local img = gm.Image(filename, config.ImageSize):size(nil,config.ImageSize):toTensor('byte','RGB','DHW')
-
+    --local img = image.load(filename)
+    local img = gm.Image(filename):toTensor('float','RGB','DHW')
     if img == nil then
         print('Image is buggy')
         print(filename)
         os.exit()
     end 
-    return PreProcess(img)---image.compressJPG(img)
-end
-
-function NameFile(filename)
-    local name = paths.basename(filename,'JPEG')
-    local substring = string.split(name,'_')
-
-    if substring[1] == 'ILSVRC2012' then -- Validation file
-        local num = tonumber(substring[3])
-        return config.ImageNetClasses.ClassNum2Wnid[config.ValidationLabels[num]] .. '_' .. num
-    else -- Training file
-        return name
+    img = PreProcess(img)
+    if config.Compressed then
+        return image.compressJPG(img)
+    else 
+        return img
     end
-
 end
 
-function LMDBFromFilenames(charTensor,env)
-    env:open()
-    local txn = env:txn()
-    local cursor = txn:cursor()
-    for i=1,charTensor:size(1) do
-        local filename = ffi.string(torch.data(charTensor[i]))
-        local data = {Data = LoadImgData(filename), Name = NameFile(filename)}
-        
-        cursor:put(string.format('%07d',i),data, lmdb.C.MDB_NODUPDATA) 
-        if i % 1000 == 0 then
-            txn:commit()
-            print(env:stat())
-            collectgarbage()
-            txn = env:txn()
-            cursor = txn:cursor()
+    function NameFile(filename)
+        local name = paths.basename(filename,'JPEG')
+        local substring = string.split(name,'_')
+
+        if substring[1] == 'ILSVRC2012' then -- Validation file
+            local num = tonumber(substring[3])
+            return config.ImageNetClasses.ClassNum2Wnid[config.ValidationLabels[num]] .. '_' .. num
+        else -- Training file
+            return name
         end
-        xlua.progress(i,charTensor:size(1))
+
     end
-    txn:commit()
-    env:close()
 
-end
+    function LMDBFromFilenames(charTensor,env)
+        env:open()
+        local txn = env:txn()
+        local cursor = txn:cursor()
+        for i=1,charTensor:size(1) do
+            local filename = ffi.string(torch.data(charTensor[i]))
+            local data = {Data = LoadImgData(filename), Name = NameFile(filename)}
+
+            cursor:put(config.Key(i),data, lmdb.C.MDB_NODUPDATA) 
+            if i % 1000 == 0 then
+                txn:commit()
+                print(env:stat())
+                collectgarbage()
+                txn = env:txn()
+                cursor = txn:cursor()
+            end
+            xlua.progress(i,charTensor:size(1))
+        end
+        txn:commit()
+        env:close()
+
+    end
 
 
-local TrainingFiles = FileSearcher{
-    Name = 'TrainingFilenames',
-    CachePrefix = config.TRAINING_DIR,
-    MaxNumItems = 1e8,
-    CacheFiles = true,
-    PathList = {config.TRAINING_PATH},
-    SubFolders = true
-}
-local ValidationFiles = FileSearcher{
-    Name = 'ValidationFilenames',
-    CachePrefix = config.VALIDATION_DIR,
-    MaxNumItems = 1e8,
-    PathList = {config.VALIDATION_PATH}
-}
+    local TrainingFiles = FileSearcher{
+        Name = 'TrainingFilenames',
+        CachePrefix = config.TRAINING_DIR,
+        MaxNumItems = 1e8,
+        CacheFiles = true,
+        PathList = {config.TRAINING_PATH},
+        SubFolders = true
+    }
+    local ValidationFiles = FileSearcher{
+        Name = 'ValidationFilenames',
+        CachePrefix = config.VALIDATION_DIR,
+        MaxNumItems = 1e8,
+        PathList = {config.VALIDATION_PATH}
+    }
 
-local TrainDB= lmdb.env{
-    Path = config.TRAINING_DIR,
-    Name = 'TrainDB'
-}
+    local TrainDB= lmdb.env{
+        Path = config.TRAINING_DIR,
+        Name = 'TrainDB'
+    }
 
-local ValDB= lmdb.env{
-    Path = config.VALIDATION_DIR,
-    Name = 'ValDB'
-}
+    local ValDB= lmdb.env{
+        Path = config.VALIDATION_DIR,
+        Name = 'ValDB'
+    }
 
-TrainingFiles:ShuffleItems()
-LMDBFromFilenames(TrainingFiles.Data, TrainDB)
-LMDBFromFilenames(ValidationFiles.Data, ValDB)
+    TrainingFiles:ShuffleItems()
+    --LMDBFromFilenames(ValidationFiles.Data, ValDB)
+    LMDBFromFilenames(TrainingFiles.Data, TrainDB)
 
 
